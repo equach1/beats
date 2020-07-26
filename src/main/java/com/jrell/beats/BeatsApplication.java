c71504a51d70ea29f04f31ca0752c342b0c9c171
equach1 <57925115+equach1@users.noreply.github.com>
2020-07-26 14:17:10 -0500

    Add files via upload

---
+++ src/main/java/com/jrell/beats/BeatsApplication.java
@@ -0,0 +1,14 @@
+package com.jrell.beats;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatsApplication.class, args);
+	}
+
+}
